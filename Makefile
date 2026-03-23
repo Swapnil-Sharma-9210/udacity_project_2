a50963b6d0e9bddb7996b8411bd4628fac5e56ce
Swapnil Sharma <swapnilsharma9210@gmail.com>
2026-03-24 03:42:53 +0530

    updated files

--- Makefile
+++ Makefile
@@ -9,6 +9,6 @@ test:
 	python -m pytest -vv test_hello.py
 
 lint:
-	pylint --disable=R,C,W1203,W0702 app.py
+	pylint --disable=R,C,W1203,W0702,W0718 app.py hello.py
 
 all: install lint test
--- commands.sh
+++ commands.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/env bash
-# Run this in Azure Cloud Shell after cloning the repo
+#!/bin/bash
+APP_NAME="swapnil-ml-app"
+RESOURCE_GROUP="swapnil-group"
 
-git clone https://github.com/Swapnil-Sharma-9210/udacity_project_2.git
-cd udacity_project_2
-python3 -m venv ~/.udacity-project2
-source ~/.udacity-project2/bin/activate
-make all
-az webapp up -n swapnil-udacity-project2 -g Azuredevops --sku FREE --runtime "PYTHON:3.8"
+az group create --name $RESOURCE_GROUP --location eastus
+
+az webapp up \
+  --name $APP_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --runtime "PYTHON|3.10"
+
+echo "App deployed at: https://$APP_NAME.azurewebsites.net"
